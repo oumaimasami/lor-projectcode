522d429d673359bc386f5d33a359a920d137798a
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-13 11:57:35 +0100

    <<<<<<<<

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
-
+import ReactPlayer from "react-player";
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <header>
+        <div class="video">
+          <video autoplay>
+            <source src="https://fb.watch/b8CJS0T_Me/" type="video/mp4" />
+          </video>
+        </div>
+      </header>
     </div>
   );
 }
