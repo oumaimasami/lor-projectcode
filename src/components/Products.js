d69449efa0fa9b0252b076e172355aa89bef66e0
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-13 21:41:23 +0100

    <<<<<<<

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,8 +2,9 @@ import React from "react";
 import "./Products.css";
 export default function Products() {
   return (
-    <div className="container">
-      <h1 className="d-flex"> Our Products</h1>
+    <div className="container my-4">
+      <h2 className="d-flex my-4"> Our Products</h2>
+      <img src="" alt="" />
     </div>
   );
 }
