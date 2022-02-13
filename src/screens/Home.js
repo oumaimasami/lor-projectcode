89905a1aa4fb0f8a59ed5a057b5b3a36cd75c7e8
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-13 20:18:04 +0100

    <<<<

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
 import ReactPlayer from "react-player";
+import video from "../images/vd.mp4";
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <header>
-        <div class="video">
-          <video autoplay>
-            <source src="https://fb.watch/b8CJS0T_Me/" type="video/mp4" />
-          </video>
-        </div>
-      </header>
+      <div className="vd">
+        <ReactPlayer
+          url={video}
+          width="100%"
+          height="100%"
+          playing={true}
+          loop={true}
+        />
+      </div>
     </div>
   );
 }
