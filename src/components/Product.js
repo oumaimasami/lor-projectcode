c51801f9f265302660bed3ec2a08b4fff007a313
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-13 21:50:59 +0100

    <<<<<<<<<

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,15 @@
 import React from "react";
+import product from "../images/Product1.jpg";
 import "./Product.css";
 export default function Product() {
-  return <div>Product</div>;
+  return (
+    <div className="row ">
+      <img className=" my-4 col-md-4" src={product} alt="" />
+      <img className=" my-4 col-md-4" src={product} alt="" />
+      <img className=" my-4 col-md-4" src={product} alt="" />
+      <img className=" my-4 col-md-4" src={product} alt="" />
+      <img className=" my-4 col-md-4" src={product} alt="" />
+      <img className=" my-4 col-md-4" src={product} alt="" />
+    </div>
+  );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Product from "./Product";
 import "./Products.css";
+
 export default function Products() {
   return (
-    <div className="container my-4">
-      <h2 className="d-flex my-4"> Our Products</h2>
-      <img src="" alt="" />
+    <div className="container my-4 ">
+      <h2 className="d-flex justify-content-center my-4"> Our Products</h2>
+      <Product />
     </div>
   );
 }
