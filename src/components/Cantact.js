fabb89e42af31811af04e2fd1d402e0d548ce081
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-16 11:33:21 +0100

    xxxx

--- src/components/Cantact.js
+++ src/components/Cantact.js
@@ -1,5 +1,38 @@
-import React from "react";
+import emailjs from "emailjs-com";
+import React, { useRef } from "react";
 import "./Cantact.css";
 export default function Cantact() {
-  return <div></div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_65fygts",
+        "template_sch6xys",
+        form.current,
+        "user_rhCMLoZOAWhNP1xIB23kW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
