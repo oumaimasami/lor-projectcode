3072bde494f9747523ac4df3a9bfeb89e7766ea2
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-16 10:46:45 +0100

    <<<<<

---
+++ src/components/Cantact.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Cantact.css";
+export default function Cantact() {
+  return <div></div>;
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import About from "../components/About";
+import Cantact from "../components/Cantact";
 import Header from "../components/Header";
 import Navbar from "../components/navbar/Navbar";
 import Products from "../components/Products";
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <Products />
       <About />
+      <Cantact />
     </>
   );
 }
