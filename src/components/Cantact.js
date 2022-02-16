e0af9b5f1cec71f0c337fca7577384b7c5c2f55f
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-16 12:05:15 +0100

    <<<<

--- src/components/Cantact.js
+++ src/components/Cantact.js
@@ -25,14 +25,41 @@ export default function Cantact() {
     e.target.reset();
   };
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <div className="container d-flex justify-content-center">
+      <form className="col-12" ref={form} onSubmit={sendEmail}>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label for="inlineFormInput">Name</label>
+            <input
+              type="name"
+              className="form-control"
+              placeholder="name"
+              type="text"
+              name="user_name"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail4"
+              placeholder="Email"
+              name="user_email"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleFormControlTextarea1">Message</label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+              name="message"
+            />
+          </div>
+          <input type="submit" value="Send" />
+        </div>
+      </form>
+    </div>
   );
 }
