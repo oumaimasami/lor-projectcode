429132891d9da47e86c8b9bc618ea8ecb596952d
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2022-02-23 11:31:46 +0100

    <<<<

---
+++ src/components/fakedata/Produits.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    description:
+      "Découvrez notre étole en soie, rayée, confectionnée à la main avec amour et passion",
+    title: "étole",
+    image: "../images/Product22.jpg",
+  },
+  {
+    description:
+      "Découvrez notre étole en soie, rayée, confectionnée à la main avec amour et passion",
+    title: "étole",
+    image: "../images/Product22.jpg",
+  },
+  {
+    description:
+      "Découvrez notre étole en soie, rayée, confectionnée à la main avec amour et passion",
+    title: "étole",
+    image: "../images/Product22.jpg",
+  },
+];
